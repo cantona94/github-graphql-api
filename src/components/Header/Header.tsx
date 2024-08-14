79693c137feaa6868ac30bcd56c24ccbf84aafc6
cantona94 <vlstan@mail.ru>
2024-08-14 15:05:18 +0500

    add Header component, refactoring

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+import styles from './Header.module.scss';
+
+type Props = {
+  setSearchString: (value: string) => void;
+};
+
+export const Header = ({ setSearchString }: Props) => {
+  const [valueInput, setValueInput] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValueInput(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchString(valueInput);
+  };
+
+  return (
+    <form className={styles.search} onSubmit={handleSubmit}>
+      <TextField
+        value={valueInput}
+        onChange={handleChange}
+        placeholder="Введите поисковый запрос"
+        variant="outlined"
+        className={styles.textField}
+        InputProps={{
+          className: styles.input,
+        }}
+      />
+      <Button variant="contained" className={styles.button} type="submit">
+        Искать
+      </Button>
+    </form>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,12 @@
-import { useQuery } from '@apollo/client';
-import { GET_REPOSITORIES } from '../api/getRepos';
+import { useState } from 'react';
+import { Header } from '../components';
 
 export const HomePage = () => {
-  const searchString = 'react';
+  const [searchString, setSearchString] = useState<string>('')
 
-  const { data, loading, error } = useQuery(GET_REPOSITORIES, {
-    variables: { searchString, repoCount: 10 },
-  });
-
-  if (loading) return null;
-  if (error) return `Error! ${error}`;
   return (
     <>
-      {data.search.repos.map((el: any, index: number) => (
-        <div key={index}>{el.name}</div>
-      ))}
+      <Header setSearchString={setSearchString} />
     </>
   );
 };
